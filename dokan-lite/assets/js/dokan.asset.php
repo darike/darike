4d316a8351f8efe53faf1a3be03fc19156e8f5db
muhammadsaif9162 <muhammadsaif9162@gmail.com>
2025-03-12 08:25:33 +0500

    hamza

---
+++ dokan-lite/assets/js/dokan.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '36359349beb9a73d2be7');
---
+++ dokan-lite/assets/js/vue-bootstrap.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'moment', 'wp-i18n'), 'version' => 'eb9095b919843ab69cd8');
